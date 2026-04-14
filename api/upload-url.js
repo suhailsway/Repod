8bf3b7cbb606dedbfa079112daddc207c78b6532
suhailsway <sohail31314@gmail.com>
2026-04-13 19:38:04 -0500

    revert to path style for R2

--- api/upload-url.js
+++ api/upload-url.js
@@ -10,7 +10,7 @@ const s3 = new S3Client({
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
   },
-  forcePathStyle: false,
+  forcePathStyle: true,
   requestChecksumCalculation: "WHEN_REQUIRED",
   responseChecksumValidation: "WHEN_REQUIRED",
 });
